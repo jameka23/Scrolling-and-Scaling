11432b53fd90be55990a551d32fd9a24ee677ec9
Jameka Echols <jameka.echols@gmail.com>
2019-03-06 13:51:41 -0600

    added the appropriate js

--- audrey.js
+++ audrey.js
@@ -6,16 +6,19 @@ const audrey = document.getElementById("audrey")
     Add an event listener to the `document` object to listen
     for the "scroll" event.
 */
-audrey.addEventListener("scroll", function () {
+window.addEventListener("scroll", function () {
     /*
         Adjust the width of audrey to be 1/3 the value of
         `window.scrollY`. No lower than 50px, though.
     */
 
-
+    
+    audrey.style.width = window.scrollY/3 + 'px'
 
     /*
         Adjust the height of audrey to be 1/4 the value of
         `window.scrollY`. No lower than 100px, though.
     */
+
+   audrey.style.height = window.scrollY/3 + 'px'
 })
